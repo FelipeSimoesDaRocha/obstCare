886b30aaca1aa3a773f9874a07bb411179d532d8
FelipeSimoesDaRocha <Felipe.simoesdarocha@gmail.com>
2023-07-09 19:39:14 -0300

    rota

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,9 +7,9 @@ import { DataItemGestantes, DataItemObstetras } from 'models'
 export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL })
 
 // Obstetras
-export const getObstetras = () => api.get<DataItemObstetras[]>('obstetras')
-export const deleteObstetras = () => api.delete<DataItemObstetras[]>('doctors')
+export const getObstetras = () => api.get<DataItemObstetras[]>('admin/obstetras')
+export const deleteObstetras = () => api.delete<DataItemObstetras[]>('admin/doctors')
 
 // Gestants
-export const getGestantes = () => api.get<DataItemGestantes[]>('gestantes')
-export const deleteGestantes = () => api.delete<DataItemObstetras[]>('pacient')
+export const getGestantes = () => api.get<DataItemGestantes[]>('admin/gestantes')
+export const deleteGestantes = () => api.delete<DataItemObstetras[]>('admin/pacient')
